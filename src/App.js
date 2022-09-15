26b4c3221205e73e1fab05a56e94865c3dd8f310
jxmauricio <johnmauricio0308@gmail.com>
2022-09-15 13:09:35 -0700

    Netlify deploy

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path ="/johnmauricio" element= {<Home/>} />
+          <Route path ="/" element= {<Home/>} />
           <Route path='/projects' element = {<Projects/>}/>
           <Route path ='/projects/:id' element = {<ProjectDisplay/>}/>
           <Route path='/experience' element = {<Experience/>}/>
