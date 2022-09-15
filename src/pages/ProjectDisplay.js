934b4f8fa293aeaaa09bbedb6efefba99937eaed
jxmauricio <johnmauricio0308@gmail.com>
2022-09-15 13:26:13 -0700

    awesome

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -10,7 +10,7 @@ function ProjectDisplay() {
   return (
     <div className = 'project'>
         <h1> {project.name}</h1>
-        <img src = {project.image} alt = 'picture of project'/>
+        <img src = {project.image} alt = 'MY PROJECT'/>
         <p>
             <b>Skills: </b>{project.skills}
         </p>
